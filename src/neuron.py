5209f5e93e10571410a84543dbbe593bc59f343d
Yosuke Ueno <youten622@yahoo.co.jp>
2019-07-06 13:54:02 +0900

    add readout()

--- src/neuron.py
+++ src/neuron.py
@@ -29,9 +29,12 @@ class Network:
         self.D = np.ones((height * width, height * width )) * 5
         self.in_indexes = np.zeros((height * width, height * width))
         self.spike_map = np.zeros((height, width))
+        self.AER = np.zeros((1, height, width))
         self.spike_map_noupdate = np.zeros((height, width))
         self._terminated = False
+        self.start = start
         self.goal = goal
+        self.SPIKED = 9
 
         self.neurons = [[0 for j in range(width)] for i in range(height)]
         for i in range(height):
@@ -67,11 +70,55 @@ class Network:
                 self.d[index] = np.maximum(self.d[index] - 1, 0)
                 if neuron.spike():
                     self.d[:, index] = self.D[:, index]
-                    self.spike_map[i][j] = 9
-                    self.spike_map_noupdate[i][j] = 9
-                    if self.spike_map[self.goal] == 9:
+                    self.spike_map[i][j] = self.SPIKED
+                    self.spike_map_noupdate[i][j] = self.SPIKED
+                    if self.spike_map[self.goal] == self.SPIKED:
                         self._terminated = True
                 self.D[index,:] += self.lr * (map_in[i, j] - self.D[index, :])
+        self.AER = np.concatenate([self.AER, self.spike_map[np.newaxis, :, :]])
+        #self.spike_map[self.start] = 10
+        #self.spike_map[self.goal] = 10
+
+    def readout(self):
+        AER = self.AER
+        path_map = np.zeros_like(AER[0])
+        all_time_steps = AER.shape[0]
+        width = AER.shape[1]
+        height = AER.shape[2]
+
+        current_point = np.array(self.goal)
+        start_point = np.array(self.start)
+        path_map[current_point[0],current_point[1]] = self.SPIKED
+        for t in reversed(range(0, all_time_steps)):
+            spike_map = AER[t - 1]
+            distance = 10000000
+            candidate = current_point
+            for _x in range(current_point[1] - 1, current_point[1] + 1 + 1):
+                if _x < 0 or _x >= width:
+                    continue
+                for _y in range(current_point[0] - 1, current_point[0] + 1 + 1):
+                    if _y < 0 or _y >= height:
+                        continue
+                    if _y == current_point[0] and _x == current_point[1]:
+                        continue
+
+                    if spike_map[_y][_x] == self.SPIKED:
+                        print(t, _y, _x)
+                        temp_point = np.array([_y, _x])
+                        temp_dist = np.linalg.norm(temp_point - start_point)
+                        if temp_dist < distance:
+                            distance = temp_dist
+                            candidate = temp_point
+            current_point = candidate
+            path_map[current_point[0], current_point[1]] = self.SPIKED
+            if (current_point == start_point).all():
+                print(current_point)
+                print(start_point)
+                print("readout break")
+                break
+        return path_map
+
+
 
 
     @property
